7c98df05fa95cf25e75b1a920d76f7c79c1367a1
Adam Smith <adsmith@nottheeconomist.com>
2017-02-15 15:35:45 -0800

    Begun work on symmetricorder

---
+++ symmetricorder/symmetricorder.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+	testCases := [][]string{
+		{"Bo",
+			"Pat",
+			"Jean",
+			"Kevin",
+			"Claude",
+			"William",
+			"Marybeth"},
+		{"Jim",
+			"Ben",
+			"Zoe",
+			"Joey",
+			"Frederick",
+			"Annabelle"}}
+}
