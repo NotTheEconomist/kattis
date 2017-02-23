3e5a7dbfc95956f59d3e6aac40346b224c7759d2
Adam Smith <adsmith@nottheeconomist.com>
2017-02-23 13:13:43 -0800

    Added solution for symmetricorder

--- symmetricorder/symmetricorder.go
+++ symmetricorder/symmetricorder.go
@@ -1,19 +1,39 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	testCases := [][]string{
-		{"Bo",
-			"Pat",
-			"Jean",
-			"Kevin",
-			"Claude",
-			"William",
-			"Marybeth"},
-		{"Jim",
-			"Ben",
-			"Zoe",
-			"Joey",
-			"Frederick",
-			"Annabelle"}}
+	var N int
+
+	for count := 1; true; count++ {
+		fmt.Scanln(&N)
+		if N == 0 {
+			break
+		}
+		names := make([]string, N)
+		resortedNames := make([]string, 0, N)
+		for i := 0; i < N; i++ {
+			fmt.Scanln(&names[i])
+		}
+
+		var start int
+		if len(names)%2 == 0 {
+			start = N - 1
+		} else {
+			start = N - 2
+		}
+
+		for i := 0; i < N; i += 2 {
+			resortedNames = append(resortedNames, names[i])
+		}
+		for i := start; i > 0; i -= 2 {
+			resortedNames = append(resortedNames, names[i])
+		}
+
+		fmt.Printf("SET %d\n", count)
+		for _, name := range resortedNames {
+			fmt.Println(name)
+		}
+	}
 }
